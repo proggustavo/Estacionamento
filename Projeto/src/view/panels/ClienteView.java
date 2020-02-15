8258c4e3c8de03bc3aa5a3dcd7c2b614166b8b23
Christian Felipe Ferreira de Lima <christian.ff@hotmail.com>
2020-02-15 18:09:25 -0300

    new class

---
+++ Projeto/src/view/panels/ClienteView.java
@@ -0,0 +1,43 @@
+package view.panels;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ClienteView {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClienteView window = new ClienteView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ClienteView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
